11cc1f7a8c020efd9f4ea0b8222719247c8a110d
Kiran-255666 <97156318+Kiran-255666@users.noreply.github.com>
2024-01-08 20:17:07 +0530

    Create KafkaConsumerExample.java

---
+++ KafkaConsumerExample.java
@@ -0,0 +1,34 @@
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConsumerExample {
+    public static void main(String[] args) {
+        // Set up consumer properties
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // Create Kafka consumer
+        Consumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe to a topic
+        String topic = "test-topic";
+        consumer.subscribe(Collections.singletonList(topic));
+
+        // Poll for messages
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            records.forEach(record -> {
+                System.out.println("Received message: Key = " + record.key() + ", Value = " + record.value());
+            });
+        }
+    }
+}
